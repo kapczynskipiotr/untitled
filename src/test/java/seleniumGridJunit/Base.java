87d30dc15f43e5c49c3dbc1bf57aabf27010b6c0
Piotrek <kapczynskipiotr@outlook.com>
2020-12-20 21:00:42 +0100

    commit

--- src/test/java/seleniumGridJunit/Base.java
+++ src/test/java/seleniumGridJunit/Base.java
@@ -18,20 +18,20 @@ public class Base extends BrowserStackConfig{
     public static void initialization() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Piotrek\\IdeaProjects\\SeleniumJava.com\\src\\resources\\driver\\chromedriver.exe");
 
-//        DesiredCapabilities cap = DesiredCapabilities.chrome();
-//        cap.setCapability("os", "Windows");
-//        cap.setCapability("os_version", "10");
-//        cap.setCapability("browser", "Chrome");
-//        cap.setCapability("browser_version", "80");
-//
-//
-//        cap.setCapability("name", "piotrkapczyski1's First Test");
-//        cap.setPlatform(Platform.WINDOWS);
-//
-//
-//        driver = new RemoteWebDriver(new URL(URL), cap);
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        cap.setCapability("os", "Windows");
+        cap.setCapability("os_version", "10");
+        cap.setCapability("browser", "Chrome");
+        cap.setCapability("browser_version", "80");
 
-        driver = new ChromeDriver();
+
+        cap.setCapability("name", "piotrkapczyski1's First Test");
+        cap.setPlatform(Platform.WINDOWS);
+
+
+        driver = new RemoteWebDriver(new URL(URL), cap);
+
+//        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
     }
